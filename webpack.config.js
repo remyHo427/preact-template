bb53da7782721d9f43f475be838b146b39c073ec
Remy Ho <remyho427@gmail.com>
2022-03-15 14:40:17 +0800

    minimal version, removed styling

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ module.exports = {
         loader: 'source-map-loader',
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
